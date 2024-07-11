0241fc10048c0cec74a14c4b8ad3d15745609e77
Bhuvi21 <bhuvijai2006@gmail.com>
2024-07-11 14:36:14 +0530

    Add files via upload

---
+++ Find Even or Odd.c
@@ -0,0 +1,18 @@
+//Even or Odd
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    printf("Enter a number: ");
+    scanf("%d",&a);
+    if (a%2==0)
+    {
+        printf("Even");
+    }
+    else
+    {
+        printf("Odd");
+    }
+    
+}
